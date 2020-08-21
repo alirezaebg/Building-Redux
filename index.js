0a269babab86cc1e8c95b1fabf3b53d8c4181cd0
Alireza Ebadi Ghajari <alirezaebg@gmail.com>
2020-08-21 12:42:35 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function createStore() {
+    /*
+        The store should have four parts:
+        1. The state
+        2. Get the state
+        3. Listen to state changes
+        4. Update the state
+    */
+   
+
+
+}
