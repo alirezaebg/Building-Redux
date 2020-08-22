e5d6836296c01b86b713edd6cf39eed8bb09859c
Alireza Ebadi Ghajari <alirezaebg@gmail.com>
2020-08-22 02:52:56 -0700

    Add getStatus and subscribe methods

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ function createStore() {
         3. Listen to state changes
         4. Update the state
     */
-   
 
+    let state
+    let listeners = []
 
+    const getState = () => state
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+    
+    return {
+        getState,
+        subscribe
+    }
+    
 }
